eeba5c7cab05d3ae1eba08fee1673ef65603ce57
hassanrawan692@gmail.com <hassanrawan692@gmail.com>
2024-03-01 14:00:04 +0200

    added backend

---
+++ backend/backend/Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Context
+{
+    public class ApplicationDbContext:DbContext
+
+
+    //Entity Framework is an Object-Relational Mapping (ORM) framework for working with databases in .NET applications
+    //The DbContext class is a part of Entity Framework and is used to interact with a database
+    //It represents a session with the database, providing a set of methods to query
+
+
+    {
+
+        //1.create a class that inherits from DbContext and represents the database context for the application.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+    }
+}
---
+++ backend/backend/Controllers/ProductsController.cs
@@ -0,0 +1,109 @@
+﻿using backend.Context;
+using backend.Dtos;
+using backend.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+
+    {
+        //Declares a private field named _context of type ApplicationDbContext
+        private readonly ApplicationDbContext _context; //PRESS ctrl+. to generate constructor quickly
+
+        //In the Web API controllers,injecting an instance of the DbContext allows to use the DbContext in the API methods
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //CRUD 
+        //Create
+        [HttpPost]//method
+        public async Task<IActionResult> CreateProduct([FromBody]CreateUpdateProductDto dto
+            )
+        {
+            var newProduct = new ProductEntity() {
+                Brand = dto.Brand,
+                Title= dto.Title,   
+            };
+
+            await _context.Products.AddAsync(newProduct);
+            await _context.SaveChangesAsync();
+
+            return Ok("Product Saved Successfully");
+
+        }
+
+
+        //Read
+        [HttpGet]
+        public async Task<ActionResult<List<ProductEntity>>> GetAllProducts()
+        {
+            var products = await _context.Products.OrderByDescending(q=>q.UpdatedAt).ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<ProductEntity>> GetProductByID([FromRoute] long id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(q => q.Id == id);
+            if(product is null) 
+            {
+                return NotFound("Product Not Found");
+            }
+
+            return Ok(product);
+        }
+
+
+        //Update
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] long id, [FromBody] CreateUpdateProductDto dto)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(q => q.Id == id);
+            if (product is null)
+            {
+                return NotFound("Product Not Found");
+            }
+
+            product.Title = dto.Title;
+            product.Brand = dto.Brand;
+            product.UpdatedAt=DateTime.Now;
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Product Updated Successfully");
+
+
+        }
+
+
+        //Delete
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult>DeleteProduct([FromRoute] long id)
+        {
+
+            var product = await _context.Products.FirstOrDefaultAsync(q => q.Id == id);
+            if (product is null)
+            {
+                return NotFound("Product Not Found");
+            }
+
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Product deleted successfully");
+
+        }
+
+    }
+}
